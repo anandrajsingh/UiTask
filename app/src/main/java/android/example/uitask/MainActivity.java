27c99f801acc4efd9c8b55a1b8af239a34c6a965
anandrajsingh <ayushpratap253@gmail.com>
2022-07-20 10:34:09 +0530

    initial commit

---
+++ app/src/main/java/android/example/uitask/MainActivity.java
@@ -0,0 +1,28 @@
+package android.example.uitask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
